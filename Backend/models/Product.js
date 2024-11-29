e4752e9757fb31acc083ee59d486d67385d75414
Raman Kishore K <ramankishore08@gmail.com>
2024-11-29 08:25:08 +0530

    Update Product.js

--- Backend/models/Product.js
+++ Backend/models/Product.js
@@ -1,6 +1,5 @@
 const mongoose = require('mongoose');
 
-// Define the product schema
 const productSchema = new mongoose.Schema({
     productName: { type: String, required: true },
     time: { type: String, required: true },
@@ -8,9 +7,6 @@ const productSchema = new mongoose.Schema({
     quantity: { type: Number, required: true },
     netPrice: { type: Number, required: true }
 });
-
-// Create a model from the schema
 const Product = mongoose.model('Product', productSchema);
 
-// Export the Product model
 module.exports = Product;
